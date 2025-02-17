d251a6ee73faa63b3ca3321d3df0f77122b4a774
LeeMangold <lmangold@outlook.com>
2025-02-16 20:59:02 -0500

    WIP

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -2,11 +2,9 @@
 
 namespace Database\Seeders;
 
-//use App\Models\Permission;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-//use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolePermissionSeeder extends Seeder
