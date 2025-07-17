a8c5a971cf3dd10ab04140bf0d073656f2733fc2
LeeMangold <lmangold@outlook.com>
2025-07-16 20:46:30 -0400

    WIP: Admin panel

---
+++ resources/views/filament/app/sidebar-bottom-links.blade.php
@@ -0,0 +1,11 @@
+<div class="fi-sidebar-bottom-links flex flex-col gap-2 mt-auto p-4">
+    <hr class="border-t-1 my-2">
+    <a href="/admin/settings" class="flex items-center gap-2 text-sm text-primary-600 hover:underline">
+        <x-heroicon-o-cog class="w-5 h-5" />
+        Settings
+    </a>
+    <a href="https://docs.opengrc.com" target="_blank" class="flex items-center gap-2 text-sm text-primary-600 hover:underline">
+        <x-heroicon-o-question-mark-circle class="w-5 h-5" />
+        Help
+    </a>
+</div> 
