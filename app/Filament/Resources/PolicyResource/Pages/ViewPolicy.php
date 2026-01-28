a97121d84150bfa579a964c6e5f0d90dd4c0fc27
Lee Mangold <lmangold@outlook.com>
2026-01-27 20:36:55 -0500

    Add policy download button on view page
    Closes OG-141

--- app/Filament/Resources/PolicyResource/Pages/ViewPolicy.php
+++ app/Filament/Resources/PolicyResource/Pages/ViewPolicy.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\PolicyResource\Pages;
 
 use App\Filament\Resources\PolicyResource;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Enums\Size;
 use Illuminate\Contracts\Support\Htmlable;
 
 class ViewPolicy extends ViewRecord
@@ -21,6 +23,28 @@ class ViewPolicy extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('download_policy')
+                ->label('Download PDF')
+                ->icon('heroicon-o-document-arrow-down')
+                // ->size(Size::Small)
+                ->color('primary')
+                ->action(function () {
+                    $policy = $this->getRecord();
+
+                    $pdf = Pdf::loadView('reports.policy', [
+                        'policy' => $policy,
+                    ]);
+
+                    $filename = $policy->code.'-'.str_replace(' ', '_', $policy->name).'-'.date('Y-m-d').'.pdf';
+
+                    return response()->streamDownload(
+                        function () use ($pdf) {
+                            echo $pdf->output();
+                        },
+                        $filename,
+                        ['Content-Type' => 'application/pdf']
+                    );
+                }),
             Action::make('view_details')
                 ->label('View Details')
                 ->url(fn () => route('filament.app.resources.policies.view-details', $this->getRecord()))
